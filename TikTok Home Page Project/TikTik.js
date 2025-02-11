2e7cec867c8ebed67f48abe4d3ea911ae816ac5b
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2025-02-11 12:53:38 -0500

    TikTok Sample Home Page

---
+++ TikTok Home Page Project/TikTik.js
@@ -0,0 +1,7 @@
+// Example script for like functionality
+document.querySelectorAll('.video-card').forEach((card) => {
+    card.addEventListener('click', () => {
+      alert('Video clicked!');
+    });
+  });
+  
